6c1c017c2da4d297caaa02a00c78bfcf9b3c8154
Pau1oo <bukato_pavel@mail.ru>
2024-09-30 01:45:58 +0300

    repo

---
+++ Infrastructure/FilmLens.DataAccess/Common/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using FilmLens.AppServices.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLens.DataAccess.Common
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
